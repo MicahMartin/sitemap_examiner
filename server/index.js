197df2edfc435de993f5206c73d4bb4a5f824153
MicahMartin <micah.m329@gmail.com>
2023-08-20 00:27:06 -0400

    sleep

--- server/index.js
+++ server/index.js
@@ -1,38 +1,46 @@
+import { 
+  validateRequest, 
+  fetchAndStoreData, 
+  closeDb, 
+  getBySku 
+} from './util/server_util.js';
 import express from 'express';
-import {validateRequest, fetchAndStoreData, closeDb, getBySku} from './util/server_util.js';
 import packageJson from './package.json' assert { type: "json" };
 
-
+// Fetch and store data before starting the server
 await fetchAndStoreData();
-
 const server = express();
+
+// Define middleware functions
 const middleware = [validateRequest];
 
+// Define a route for handling product requests by SKU
 server.get("/product/:sku", middleware, async (req, res, next) => {
   try {
     const sku = req.params["sku"];
     const product = await getBySku(sku);
-    if(!product){
+    // Set HTTP status code to 404 if product is not found
+    if (!product) {
       res.status(404);
-      res.json({message: "404 sku not found"});
+      res.json({ message: "404 sku not found" });
     }
-    res.json(product);
+    res.json(product); // Send the retrieved product details as JSON response
   } catch (e) {
-    next(e);
+    next(e); // Call the error handling middleware in case of an exception
   }
 });
 
-// Status Page
+// Define a route for the status page
 server.get("/status", async (req, res) => {
   const statusObj = {
     server_port: PORT,
-    server_version: packageJson.version,
+    server_version: packageJson.version, // Include the server version from package.json in the status object
   };
 
   res.json(statusObj);
 });
 
-// Error Page
+// Define a global error handling middleware
 server.use((err, req, res, next) => {
   console.error(err);
   res.status(err.status || 500).json({ status: err.status, message: err.message });
@@ -41,7 +49,7 @@ server.use((err, req, res, next) => {
 const PORT = process.env.PORT || 8032;
 server.listen(PORT);
 
+// Attach an exit event listener to close the database connection before exiting
 process.on('exit', function() {
-  console.log('About to close');
   closeDb();
 });
--- server/util/server_util.js
+++ server/util/server_util.js
@@ -5,10 +5,12 @@ import { parseString } from 'xml2js';
 import fs from 'fs';
 import redis from 'redis';
 
-// We could use redis for both data store & cache, didn't really need mysql
+// Create a Redis cache client instance using default redis port 6379
 const cache = redis.createClient();
 await cache.connect();
 
+// Create an in-memory SQLite database and set up the products table
+// We could definitely use redis for this too
 const db = new Database(':memory:', {});
 db.pragma('journal_mode = WAL');
 db.prepare(`
@@ -19,16 +21,23 @@ db.prepare(`
   )
 `).run();
 
-// store data in sqlite with a sku column for constant lookup time
+const checkDuplicates = db.prepare(`
+  SELECT sku,
+  COUNT(*) c
+  FROM products
+  GROUP BY sku HAVING c > 1
+`);
+
+// Function to fetch and store data from sitemap
+// We spend some extra time setting this up when server starts,
+// But now we get constant lookup time when searching by sku
+
 export const fetchAndStoreData = async () => {
   const SITEMAP_URL = process.env.SITEMAP_URL || "https://www.christianbook.com/sitemap4.xml";
   try {
-    // Check if sitemap4.xml exists on disk, 
-    // if it doesn't then download it
-    if(!fs.existsSync("./data/sitemap4.xml")){
-      const { data } = await axios.get(SITEMAP_URL, {
-        responseType: "arraybuffer",
-      });
+    // Check if sitemap4.xml exists on disk, if not, download and save it
+    if (!fs.existsSync("./data/sitemap4.xml")) {
+      const { data } = await axios.get(SITEMAP_URL, { responseType: "arraybuffer" });
       fs.writeFileSync("./data/sitemap4.xml", data);
     }
 
@@ -39,78 +48,82 @@ export const fetchAndStoreData = async () => {
         return;
       }
 
-
-      // TODO: Add condition to check if item already exists
+      // Prepare an SQL statement for inserting or updating product data
       const insertStatement = db.prepare(`
-        REPLACE INTO products (sku, url)
+        INSERT INTO products (sku, url)
         VALUES (?, ?)
       `);
 
-      // sqlite uses transactions for batch processing
+      // Use a transaction for batch processing
       db.transaction(() => {
         const pattern = /\/pd\/([a-zA-Z0-9]+)/;
         const productUrls = result.urlset.url.map(url => url.loc[0]);
+
         for (const productUrl of productUrls) {
           const match = productUrl.match(pattern);
           let sku = '';
 
-          if (match && match[1]){
+          if (match && match[1]) {
             sku = match[1];
             insertStatement.run(sku, productUrl);
           }
         }
       }).call();
       console.log('Data imported successfully');
+      // sanity check for dupes
+      const rows = checkDuplicates.all();
+      console.log(rows);
     });
   } catch (error) {
     console.error('Error:', error);
   }
 };
 
+// Scrape product details from HTML
 const scrapeHtml = (html) => {
+  //FIXME: These HTML selectors are pretty fragile. This code is probably error prone
   const $ = load(html);
-  // TODO: More specific HTML selector. 
-  // this code is fragile and probalby error prone.
   const title = $('.CBD-TitleWrapper > .CBD-ProductDetailTitle').text();
   const author = $('.CBD-ProductDetailAuthor > .CBD-ProductDetailAuthorLink').first().text().trim();
   const price = $('.CBD-ProductDetailActionPrice').text().trim().match(/\$([\d.]+)/)[1]; 
-  return {title, author, price};
- };
+  return { title, author, price };
+};
 
+// Retrieve product details by SKU
 export const getBySku = async (sku) => {
-  // Get the URL from our sku DB
+  // prepared statement to grab url by sku
   const row = db.prepare(`
     SELECT * 
     FROM products 
     WHERE sku = ?
-    `).get(sku);
-  // Check Redis Cache before requesting from christianbook
+  `).get(sku);
+
+  // Check Redis Cache before requesting from Christianbook
   const cachedResult = await cache.get(sku);
-  if(cachedResult){
-    // Redis needs JSON module to store actual json, and it isnt installed by default
-    // So we're storing the JSON as a string & parsing it on retrivial
+  if (cachedResult) {
+    // Retrieve JSON from Redis and parse it
     return JSON.parse(cachedResult);
   } else {
-    // Maybe using chunking here so we don't load the whole page?
-    const { data } = await axios.get(row.url);
+    // FIXME: Use chunking so we dont have to load the whole page?
+      const { data } = await axios.get(row.url);
     const payload = scrapeHtml(data);
 
-    // Add payload to cache
+    // Add payload to cache with a one-day expiration
     // No magic numbers!
-    const oneDay = 60*60*24;
+      const oneDay = 60 * 60 * 24;
     await cache.set(sku, JSON.stringify(payload), { EX: oneDay });
     return payload;
   }
-}
+};
 
-
+// close the SQLite database
 export const closeDb = () => db.close(); 
 
-// Validate API requests
+// TODO: Middleware function to validate API requests (to be replaced with actual validation logic)
 export const validateRequest = (req, res, next) => {
   console.debug("validating request");
   if (!true) {
-    // Validation didn't pass
+    // Validation didn't pass, throw an error
     throw({ status: 401, message: "Validation failed" });
   }
   next();
